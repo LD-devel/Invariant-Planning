b2dcd43598db881fd11506d2cf887e31adcdd3c2
László Dirks <l.dirks@me.com>
2021-05-10 12:49:42 +0200

    test-suit works

--- test-suit/test.py
+++ test-suit/test.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import matplotlib.pyplot as plt
+from natsort import natsorted
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 val_path = '/bin/validate'
@@ -17,8 +18,47 @@ from planner import search
 
 def main():
 
-    problems = [('zeno-travel-linear', 'pddl_examples\linear\zeno-travel-linear\domain.pddl',
-     '.\pddl_examples\linear\zeno-travel-linear\instances',1)]
+    problems = [('zeno-travel-linear', r'pddl_examples\linear\zeno-travel-linear\domain.pddl',
+     r'pddl_examples\linear\zeno-travel-linear\instances',7),
+     ('farmland_ln', r'pddl_examples\linear\farmland_ln\domain.pddl',
+     r'pddl_examples\linear\farmland_ln\instances',0),
+     ('fo_counters', r'pddl_examples\linear\fo_counters\domain.pddl',
+     r'pddl_examples\linear\fo_counters\instances',10),
+     ('fo_counters_inv', r'pddl_examples\linear\fo_counters_inv\domain.pddl',
+     r'pddl_examples\linear\fo_counters_inv\instances',10),
+     ('fo_counters_rnd', r'pddl_examples\linear\fo_counters_rnd\domain.pddl',
+     r'pddl_examples\linear\fo_counters_rnd\instances',10),
+     ('sailing_ln', r'pddl_examples\linear\sailing_ln\domain.pddl',
+     r'pddl_examples\linear\sailing_ln\instances',0),
+     ('tpp', r'pddl_examples\linear\tpp\domain.pddl',
+     r'pddl_examples\linear\tpp\instances',4),
+     ('depots_numeric', r'pddl_examples\simple\depots_numeric\domain.pddl',
+     r'pddl_examples\simple\depots_numeric\instances',3),
+     ('gardening', r'pddl_examples\simple\gardening\domain.pddl',
+     r'pddl_examples\simple\gardening\instances',5),
+     ('rover-numeric', r'pddl_examples\simple\rover-numeric\domain.pddl',
+     r'pddl_examples\simple\rover-numeric\instances',5)]
+
+    '''problems = [('zeno-travel-linear', r'pddl_examples\linear\zeno-travel-linear\domain.pddl',
+     r'pddl_examples\linear\zeno-travel-linear\instances',1),
+     ('farmland_ln', r'pddl_examples\linear\farmland_ln\domain.pddl',
+     r'pddl_examples\linear\farmland_ln\instances',0),
+     ('fo_counters', r'pddl_examples\linear\fo_counters\domain.pddl',
+     r'pddl_examples\linear\fo_counters\instances',2),
+     ('fo_counters_inv', r'pddl_examples\linear\fo_counters_inv\domain.pddl',
+     r'pddl_examples\linear\fo_counters_inv\instances',1),
+     ('fo_counters_rnd', r'pddl_examples\linear\fo_counters_rnd\domain.pddl',
+     r'pddl_examples\linear\fo_counters_rnd\instances',1),
+     ('tpp', r'pddl_examples\linear\tpp\domain.pddl',
+     r'pddl_examples\linear\tpp\instances',0),
+     ('depots_numeric', r'pddl_examples\simple\depots_numeric\domain.pddl',
+     r'pddl_examples\simple\depots_numeric\instances',0),
+     ('gardening', r'pddl_examples\simple\gardening\domain.pddl',
+     r'pddl_examples\simple\gardening\instances',0),
+     ('rover-numeric', r'pddl_examples\simple\rover-numeric\domain.pddl',
+     r'pddl_examples\simple\rover-numeric\instances',0),
+     ('sailing_ln', r'pddl_examples\linear\sailing_ln\domain.pddl',
+     r'pddl_examples\linear\sailing_ln\instances',0)]'''
 
     # Create report
     myReport = Report()
@@ -29,8 +69,9 @@ def main():
     for domain_name, domain, instance_dir, domain_bound in problems:
         counter = 0
         abs_instance_dir = os.path.join(BASE_DIR, instance_dir)
+        #abs_instance_dir = BASE_DIR + instance_dir
 
-        for filename in os.listdir(abs_instance_dir):
+        for filename in natsorted(os.listdir(abs_instance_dir)):
             if filename.endswith('.pddl') and counter < domain_bound:
 
                 counter = counter +1
@@ -117,31 +158,51 @@ class Report():
             self.sorted[log['domain']][log['instance']][key] = log['valid']
         
         # Plot the log
-        figure = plt.figure()
-        fig, axes = plt.subplots(nrows=len(self.sorted), ncols=2, sharex=True, sharey=True, squeeze=False)
-
-        i = 0
-        for key, dom  in self.sorted.iteritems():
+        for key1, dom  in self.sorted.iteritems():
+            # New fig for each domain
+            figure = plt.figure()
+            fig, axes = plt.subplots(nrows=1, ncols=2, sharex=False, sharey=False, squeeze=False)
             x = []
             t1 = []
             t2 = []
             s1 = []
-            s3 = []
+            s2 = []
             n = 1
-            for key, ins in dom.iteritems():
+
+            #TODO make this failsafe
+            for key2, ins in dom.iteritems():
                 x.append(n)
                 n = n + 1
                 t1.append(ins['parallel_time'])
                 t2.append(ins['relaxed_time'])
+
+                s1.append(ins['parallel_steps'])
+                s2.append(ins['relaxed_steps'])
+            
             # Plot in subplot
+            axes[0,0].set_title(key1)
+            axes[0,0].set_xlabel('Instance')
+            axes[0,0].set_ylabel('t in s')
+            axes[0,1].set_xlabel('Instance')
+            axes[0,1].set_ylabel('Steps')
+
             for k in range(len(x)):
-                axes[i,0].bar(x[k]-0.2, t1[k], width=0.4, color='b', align='center')
-                axes[i,0].bar(x[k]+0.2, t2[k], width=0.4, color='g', align='center')
-            i = i+1
+                if k == 0:
+                    axes[0,0].bar(x[k]-0.2, t1[k], width=0.4, color='b', align='center',
+                        label='parallel')
+                    axes[0,0].bar(x[k]+0.2, t2[k], width=0.4, color='g', align='center',
+                        label='relaxed')
+                else:
+                    axes[0,0].bar(x[k]-0.2, t1[k], width=0.4, color='b', align='center')
+                    axes[0,0].bar(x[k]+0.2, t2[k], width=0.4, color='g', align='center')
+
+                axes[0,1].bar(x[k]-0.2, s1[k], width=0.4, color='b', align='center')
+                axes[0,1].bar(x[k]+0.2, s2[k], width=0.4, color='g', align='center')
+                
+
         
-        #plt.show()
-        figure.savefig('analysis_figure.png', bbox_inches='tight')
-        plt.savefig('analysis.png')
+            axes[0,0].legend()
+            plt.savefig(r'output/analysis_'+str(key1)+'.png')
 
 if __name__ == '__main__':
     main()
